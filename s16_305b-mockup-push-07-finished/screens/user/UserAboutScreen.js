39d7d3c8f4ff02702a28e4a7e252a813e5e8af76
John Sheehy <sheehy.john@gmail.com>
2020-07-23 12:22:37 +0100

    Styles copied from "Product Item"

--- s16_305b-mockup-push-07-finished/screens/user/UserAboutScreen.js
+++ s16_305b-mockup-push-07-finished/screens/user/UserAboutScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList, Button, Platform, Alert } from 'react-native';
+import { View, Text, FlatList, Button, Platform, Alert, StyleSheet } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -100,4 +100,48 @@ export const screenOptions = navData => {
   };
 };
 
+
+const styles = StyleSheet.create({
+    product: {
+      height: 300,
+      margin: 20
+    },
+    touchable: {
+      borderRadius: 10,
+      overflow: 'hidden'
+    },
+    imageContainer: {
+      width: '100%',
+      height: '60%',
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+      overflow: 'hidden'
+    },
+    image: {
+      width: '100%',
+      height: '100%'
+    },
+    details: {
+      alignItems: 'center',
+      height: '17%',
+      padding: 10
+    },
+    title: {
+      fontFamily: 'open-sans-bold',
+      fontSize: 18,
+      marginVertical: 2
+    },
+    price: {
+      fontFamily: 'open-sans',
+      fontSize: 14,
+      color: '#888'
+    },
+    actions: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      height: '23%',
+      paddingHorizontal: 20
+    }
+  });
 export default UserAboutScreen;
